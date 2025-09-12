1a529c762323f3684c8d48bf02aa218f58db35e7
ntthanh <ntthanh2357@gmail.com>
2025-09-12 11:47:37 +0700

    slot2

---
+++ slot2/ex2/src/test2.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, 3, 4, 5];
+arr.forEach(num => {
+    console.log(num * 2);
+});
+
+let people = [
+    { name: "An", age: 20 },
+    { name: "Binh", age: 22 },
+    { name: "Cuong", age: 25 }
+];
+
+people.forEach(person => {
+    console.log(person.name, person.age);
+});
